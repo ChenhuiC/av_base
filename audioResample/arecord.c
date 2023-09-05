d440915d1aaafb3c9113ee7b61ad38e8de2413a0
chen_hen <3111502752@qq.com>
2023-09-05 23:18:14 +0800

    1.增加音频重采样

--- audioResample/arecord.c
+++ audioResample/arecord.c
@@ -1,5 +1,5 @@
 /**
-* @file: test.c
+* @file: arecord.c
 * @brief: 
         默认采样率 44100 通道数 2 采样位数 f32lebit
 * @author: chenhui
@@ -10,12 +10,15 @@
 */
 /* includes ------------------------------------------------------------------*/
 #include "arecord.h"
+#include <string.h>
+#include "libswresample/swresample.h"
 
 /* private marcos ------------------------------------------------------------*/
 /* private types -------------------------------------------------------------*/
 /* private variables ---------------------------------------------------------*/
 static int rec_status = REC_STOP;
-
+static uint8_t rec_buf[4096];
+static uint32_t wPointer = 0;
 /* private functions ---------------------------------------------------------*/
 
 void rec_audio(void)
@@ -27,6 +30,8 @@ void rec_audio(void)
     AVDictionary *options = NULL;
     AVInputFormat *iformat = NULL;
     AVPacket pkt;
+
+    SwrContext *swr_ctx = NULL;
 
     av_log_set_level(AV_LOG_DEBUG);
     av_log(NULL, AV_LOG_DEBUG, "this is a log\n");
@@ -52,12 +57,78 @@ void rec_audio(void)
         av_log(NULL, AV_LOG_DEBUG, "fopen err\n");
         return;
     }
+
+    const int in_sample_rate = 44100;
+    enum AVSampleFormat in_sfmt = AV_SAMPLE_FMT_S16;
+    uint64_t in_channel_layout = AV_CH_LAYOUT_STEREO;
+    int in_channels = av_get_channel_layout_nb_channels(in_channel_layout);
+    const int out_sample_rate = 8000;
+    enum AVSampleFormat out_sfmt = AV_SAMPLE_FMT_S32;
+    uint64_t out_channel_layout = AV_CH_LAYOUT_STEREO;
+    int out_channels = av_get_channel_layout_nb_channels(out_channel_layout);
+
+    int in_nb_samples = sizeof(rec_buf)/2/2;       /* 输入单通道采样个数 */
+    int out_nb_samples = av_rescale_rnd(in_nb_samples,out_sample_rate,in_sample_rate,AV_ROUND_UP);      /* 输出单通道采样个数  */
+
+    swr_ctx = swr_alloc_set_opts(NULL,                  /*ctx*/
+                                out_channel_layout,     /*输出channel布局*/
+                                out_sfmt,               /*输出采样格式*/
+                                out_sample_rate,        /*采样率*/
+                                in_channel_layout,      /*输入channel布局*/
+                                in_sfmt,                /*输入的采样格式*/
+                                in_sample_rate,         /*采样率*/
+                                0,
+                                NULL);
+    if(swr_ctx == NULL){
+        av_log(NULL, AV_LOG_DEBUG, "swr_alloc_set_opts err\n");
+        return;
+    }
+
+    if(swr_init(swr_ctx) < 0){
+    
+    }
+
+    unsigned char **src_data;
+    int src_linesize = 0;
+    unsigned char **dst_data;
+    int dst_linesize = 0;
+
+
+    /* 创建输入缓冲区 */
+    av_samples_alloc_array_and_samples(&src_data,           /*输入缓冲区地址*/
+                                        &src_linesize,      /*缓冲区大小*/
+                                        in_channels,        /*通道个数*/
+                                        in_nb_samples,      /*单通道采样个数*/
+                                        in_sfmt,            /*采样格式*/
+                                        0);
+    /* 创建输出缓冲区 */
+    av_samples_alloc_array_and_samples(&dst_data,           /*输出缓冲区地址*/
+                                        &dst_linesize,      /*缓冲区大小*/
+                                        out_channels,                  /*通道个数*/
+                                        out_nb_samples,         /*单通道采样个数*/
+                                        out_sfmt,  /*采样格式*/
+                                        0);
+
     rec_status = REC_START;
 //    av_init_packet(&pkt);     /* 新的ffmpeg 都不需要初始化 */
+    memset(rec_buf,0,sizeof(rec_buf));
     while(((ret = av_read_frame(fmt_ctx,&pkt) ) == 0) && ( rec_status == REC_START )){
-        av_log(NULL, AV_LOG_INFO, "pkt size:%d(%p)\n",pkt.size,pkt.data);
-        fwrite(pkt.data, pkt.size, 1, outfile);
-        fflush(outfile);
+        // av_log(NULL, AV_LOG_INFO, "pkt size:%d(%p)\n",pkt.size,pkt.data);
+        if(wPointer + pkt.size > sizeof(rec_buf)){
+            av_log(NULL,AV_LOG_DEBUG,"wPointer:%d\n",wPointer);
+            // fwrite(rec_buf, wPointer, 1, outfile);
+            memcpy(src_data[0],rec_buf,wPointer);
+            swr_convert(swr_ctx,                        /*重采样上下文*/
+                        dst_data,                       /*输出缓冲区*/
+                        out_nb_samples,                 /*输出每个通道的采样数*/
+                        (const uint8_t **)src_data,     /*输入缓冲区*/
+                        in_nb_samples);                 /*输入每个通道的采样数*/
+            fwrite(dst_data[0], 1, dst_linesize, outfile);
+            fflush(outfile);
+            wPointer = 0;
+        }
+        memcpy(rec_buf + wPointer, pkt.data, pkt.size);
+        wPointer += pkt.size;
         av_packet_unref(&pkt);
 
     }
@@ -65,10 +136,26 @@ void rec_audio(void)
     av_log(NULL, AV_LOG_DEBUG, "rec_audio end\n");
     /*释放资源*/
     fclose(outfile);
+    if(src_data){
+        av_freep(&src_data[0]);
+    }
+    av_freep(&src_data);
+    if(dst_data){
+        av_freep(&dst_data[0]);
+    }
+    av_freep(&dst_data);
+
     avformat_close_input(&fmt_ctx);
     return;
 }
 
+
+
+/**
+ * @brief 设置录制状态
+ * 
+ * @param status 
+ */
 void set_status(int status)
 {
     rec_status = status;
---
+++ audioResample/readme.md
@@ -0,0 +1,37 @@
+
+# 采集音频
+1. 命令方式
+ffmpeg -f alsa -i hw:1,0 try.wav
+
+2. 代码方式
+默认采集率 44100 通道数 2  采样大小16bit
+
+# 播放音频
+
+1. 播放pcm
+ffplay -ar 44100 -ac 2 -f s16le test.pcm
+
+
+# 重采样
+## 用到的数据
+1. 上下文  SwrContext
+    初始化该上下文 swr_alloc_set_opts(参数...)
+## 用到的函数
+1.    int swr_convert(struct SwrContext *s, uint8_t **out, int out_count,
+                                const uint8_t **in , int in_count);
+
+
+
+# 问题及解决方法
+
+1. -alsa-调节音频音量大小
+获取各参数当前的值：
+amixer contents
+设置录音音频参数
+amixer cset numid=20,iface=MIXER,name='Capture Volume' 13
+
+2. 请教一下，例子中输入是 f32，但输出是 s16，f32 是4字节所以 输入单通道采样个数是 4096/4/2 = 512，但输出单通道采样个数为什么也是 512 ？ s16 不是应该是 2字节，4096/2/2 = 1024 吗 ？
+要保持单通道下输入与输出采样个数一样，变量是采样位深
+
+# 参考
+## 重采样参考：https://blog.csdn.net/wanggao_1990/article/details/115731502
